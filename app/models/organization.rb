9178375d44b1865ec818d1046fe5dc432a5ce5ef
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-05-28 23:43:17 +1000

    joint user-orga

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,6 @@
 class Organization < ActiveRecord::Base
   attr_accessible :name
+  
+  has_many :user_orga_rels
+  has_many :users, through: :user_orga_rels
 end
--- app/models/user_orga_rel.rb
+++ app/models/user_orga_rel.rb
@@ -1,3 +1,6 @@
 class UserOrgaRel < ActiveRecord::Base
   attr_accessible :organization_id, :user_id
+  
+  belongs_to :organization
+  belongs_to :user
 end
